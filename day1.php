8f45c6419e0329c162c5805b5e344620df9708bb
BOCKTAELS Hugo <hugobocktaels@gmail.com>
2021-12-01 18:04:13 +0100

    Update day1.php

--- day1.php
+++ day1.php
@@ -5,19 +5,14 @@
  $index = 0;
  while(!feof($file)){
      $ligne = fgets($file);
-     if( intval($ligne) > intval($lastLine) ){
-        $index++;
-    }
+     $index = intval($ligne) > intval($lastLine) ? ++$index : $index;    
     $lastLine = $ligne;
  }
  echo $index;
 
 // PART 2 
 $file = file('input.txt');
-$index = 0;
-$nbrChange = 0;
-$lastWindow = 0;
-$window = 0;
+$index = $nbrChange = $lastWindow = $window = 0;
 foreach ($file as $key => $val) {
     $window = 0;
     for ($i = $key; $i <  $key + 3; $i++) {
